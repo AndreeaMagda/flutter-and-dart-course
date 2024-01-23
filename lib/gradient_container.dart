e68445227d8ae6c5e9c0c4e7c1f63bbc8ae61eb6
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-01-23 22:48:36 +0200

    Displaying Images & Using Multiple Constructor Functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:my_project/styled_text.dart';
 
-const startAlignnnet=Alignment.topLeft;
-const endAlignment=Alignment.bottomRight;
 
-class GradientContainer extends StatelessWidget{
-  const GradientContainer(this.colorStart, this.colorEnd,{super.key});
+const startAlignnnet = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colorStart, this.colorEnd, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : colorStart = Colors.deepPurple,
+        colorEnd = Colors.indigo;
 
   final Color colorStart;
   final Color colorEnd;
-
+// method 1
   @override
-  Widget build(context){
+  Widget build(context) {
     return Container(
-          decoration:  BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                colorStart,
-                colorEnd,
-              ],
-              begin:startAlignnnet,
-              end:endAlignment ,
-            ),
-          ),
-          child:  Center(
-            child: StyledText('Expecto Patronum 🪄'),
-          ),
-        );
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            colorStart,
+            colorEnd,
+          ],
+          begin: startAlignnnet,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Image.asset('assets/images/dice-2.png',
+        width: 200,),
+      ),
+    );
   }
 } 
 
+//method 2
 
 // class GradientContainer extends StatelessWidget{
 //   const GradientContainer({super.key, required this.colors});
