07fc9d55e90d21add9fefaa34946ba95d16ee3eb
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-01-28 17:23:28 +0200

    Create welcome page

---
+++ lib/welcome_quiz.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class WelcomeQuiz extends StatelessWidget {
+  const WelcomeQuiz({super.key});
+void startQuiz(){}
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+      Image.asset(
+        'assets/images/quiz-logo.png',
+        width: 200,
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+   const   Text( 'Learn flutter the fun way',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,)
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      TextButton(onPressed:startQuiz , child: const Text('Start Quiz'))
+    ]);
+  }
+}
